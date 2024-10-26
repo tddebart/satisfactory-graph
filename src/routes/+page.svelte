cda5b109d17d40f863d77eea3c9c99253217e58e
tddebart <tddebart@gmail.com>
2024-10-26 14:13:08 +0200

    auto save

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,8 +4,8 @@
     import { browser } from '$app/environment';
     import '$lib/index';
     import '$lib/JsonLoader';
-    import { SvelteFlow, SvelteFlowProvider, Background, Controls, MiniMap } from '@xyflow/svelte';
-    import type { Node, Edge, OnConnectEnd } from '@xyflow/svelte';
+    import { SvelteFlow, SvelteFlowProvider, Background, Controls, MiniMap, useSvelteFlow } from '@xyflow/svelte';
+    import type { Node, Edge, OnConnectEnd, Viewport } from '@xyflow/svelte';
     import { writable } from 'svelte/store';
     import CustomNode from '$lib/components/CustomNode.svelte';
     import createUndoable from '$lib/Undoable';
@@ -15,6 +15,10 @@
     let onconnectend: OnConnectEnd;
     let onContextmenu: (event: MouseEvent) => void;
     let hideRightClickMenu: () => void;
+
+    const localStorageKey = 'satisfactory-graph';
+
+    const { toObject } = useSvelteFlow();
 
     const nodeTypes = {
         'custom': CustomNode,
@@ -59,6 +63,12 @@
     ]);
     const edgeUndoable = createUndoable(edges);
 
+    const viewport = writable<Viewport>({
+        x: 0,
+        y: 0,
+        zoom: 1,
+    });
+
     if (browser) {
         window.addEventListener('keydown', (e: KeyboardEvent) => {
             if ((e.key === 'y' && e.ctrlKey) || (e.key === 'Z' && e.ctrlKey && e.shiftKey)) {
@@ -67,7 +77,22 @@
             if (e.key === 'z' && e.ctrlKey) {
                 undo();
             }
+
+            if (e.key === 'a' && e.ctrlKey) {
+                $nodes.forEach((n) => (n.selected = true));
+            }
         });
+
+        if (localStorage.getItem(localStorageKey)) {
+            const { nodes: localStorageNodes, edges: localStorageEdges, viewport: localStorageViewport } = JSON.parse(
+                localStorage.getItem(localStorageKey),
+            );
+
+            console.log(localStorageViewport)
+            nodes.set(localStorageNodes);
+            edges.set(localStorageEdges);
+            viewport.set(localStorageViewport);
+        }
     }
 
     function oninit() {
@@ -78,11 +103,13 @@
     function saveHistory() {
         nodeUndoable.set(getStoreValue(nodes));
         edgeUndoable.set(getStoreValue(edges));
+        save();
     }
 
     function undo() {
         nodeUndoable.undo();
         edgeUndoable.undo();
+        save();
 
         // Sanity check
         if (nodeUndoable.historyIndex() != edgeUndoable.historyIndex()) {
@@ -95,6 +122,7 @@
     function redo() {
         nodeUndoable.redo();
         edgeUndoable.redo();
+        save();
 
         // Sanity check
         if (nodeUndoable.historyIndex() != edgeUndoable.historyIndex()) {
@@ -109,37 +137,41 @@
         $edges = $edges;
         saveHistory();
     }
+
+    function save() {
+        console.log('saving')
+        localStorage.setItem(localStorageKey, JSON.stringify(toObject()));
+    }
 </script>
 
-<SvelteFlowProvider>
-    <div
-        style="width: 100vw; height: 100vh"
-        on:contextmenu={onContextmenu}
-        on:click={() => hideRightClickMenu()}
+<div
+    style="width: 100vw; height: 100vh"
+    on:contextmenu={onContextmenu}
+    on:click={() => hideRightClickMenu()}
+>
+    <SvelteFlow
+        {nodes}
+        {edges}
+        {viewport}
+        {nodeTypes}
+        colorMode="dark"
+        fitView={$viewport.x === 0 && $viewport.y === 0}
+        onlyRenderVisibleElements
+        deleteKey="Delete"
+        {onconnectend}
+        {oninit}
+        onconnect={saveHistory}
+        ondelete={saveHistory}
+        on:nodedragstop={saveHistory}
     >
-        <SvelteFlow
-            {nodes}
-            {edges}
-            {nodeTypes}
-            colorMode="dark"
-            fitView
-            onlyRenderVisibleElements
-            deleteKey="Delete"
-            {onconnectend}
-            {oninit}
-            onconnect={saveHistory}
-            ondelete={saveHistory}
-            on:nodedragstop={saveHistory}
-        >
-            <Background />
-            <Controls />
-            <MiniMap />
-        </SvelteFlow>
-    </div>
-    <RightClickMenu
-        bind:onconnectend
-        bind:onContextmenu
-        bind:hide={hideRightClickMenu}
-        {updateGraph}
-    />
-</SvelteFlowProvider>
+        <Background />
+        <Controls />
+        <MiniMap />
+    </SvelteFlow>
+</div>
+<RightClickMenu
+    bind:onconnectend
+    bind:onContextmenu
+    bind:hide={hideRightClickMenu}
+    {updateGraph}
+/>
